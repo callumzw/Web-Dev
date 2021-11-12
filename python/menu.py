923c33a6d88fe3c3feee0b29fd76c4ff1e935dbd
callumzw <cgrant.zw@gmail.com>
2021-11-12 12:05:18 +0000

    Web App updated

--- python/menu.py
+++ python/menu.py
@@ -2,5 +2,21 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def mainMenu():
     return 'Hello Napier'
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return "Couldnt find the page you have requested", 4040
+
+@app.route('/private')
+def private()
+# check if logged in
+return redirect(url_for('login'))
+
+@app.route('/login')
+def login ():
+    return "Fill in username & password"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
