eb380119b5c5466c1b79a4fe5c37feeb88c289de
callumzw <cgrant.zw@gmail.com>
2021-11-12 13:15:39 +0000

    Configs added

--- python/menu.py
+++ python/menu.py
@@ -1,5 +1,21 @@
+import configparser
 from flask import Flask, render_template, request, url_for, redirect
 app = Flask(__name__)
+
+def init (app):
+    config = configparser.ConfigParser()
+    try :
+        config_location = "etc/test.cfg"
+        config.read(config_location)
+
+        app.config ['DEBUG'] = config.get ("config", "debug")
+        app.config ['ip_address'] = config.get ("config", "ip_address")
+        app.config ['port'] = config.get ("config", "port")
+        app.config ["url"] = config.get ("config", "url")
+    except:
+        print (" Couldn’t read configs from : ", config_location )
+
+init(app)
 
 @app.route('/')
 def mainMenu():
